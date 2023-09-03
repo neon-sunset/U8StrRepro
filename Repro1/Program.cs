dbbf6900c5688b9a722fe5a7c1cb77f7ae1e18a2
neon-sunset <neon_sunset@outlook.com>
2023-09-03 10:52:26 +0300

    add repro 1

---
+++ Repro1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.CompilerServices;
+using U8Primitives;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var str = (U8String)"Hello, World!"u8;
+
+        Console.WriteLine(StripSpace(str));
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveOptimization)]
+    private static U8String StripSpace(U8String str)
+    {
+        return str.Remove(' ');
+    }
+}
